570b7203e6d36aca890824ff6e6072e22173fd22
seungyoupbaek <bsy8220@naver.com>
2022-10-03 17:34:27 +0900

    feat(controller): remove exception hadling logic
    remove exception hadling logic in Application main function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,11 +4,6 @@ import baseball.controller.GameController;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            GameController.gameStart();
-        } catch (IllegalArgumentException e) {
-            System.out.println("잘못된 입력입니다.");
-            System.out.println("게임 종료");
-        }
+        GameController.gameStart();
     }
 }
